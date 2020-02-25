ec8e68cb6332b0e45e09a3b1d79de840f6cab007
quyhp <quyhp@eplatform.vn>
2020-02-25 17:22:20 +0700

    init project

--- fbvn-friends/content.js
+++ fbvn-friends/content.js
@@ -1,63 +1,9 @@
 
 import Common from './plugins/Common';
 
-import FriendManagement from './plugins/FriendManagement';
-import GroupModeration from './plugins/GroupModeration';
-import GroupInviteMember from './plugins/GroupInviteMember';
-
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  Features[message.command](message.data);
-});
-
-jQuery.fn.extend({
-  realClick: function() {
-    const EVENTS = ["focus", "mousedown", "mouseup", "click"];
-    return this.each(function() {
-      for (let event of EVENTS) {
-        let e = new Event(event, { view: window, bubbles: true, cancelable: false });
-        this.dispatchEvent(e);
-      }
-    });
-  }
+  Features[message.command](message.data, sendResponse, sender);
 });
 
 var Features = {
-  addFriendsAuto() {
-    FriendManagement.addFriendsAuto();
-  },
-  cancelFriendRequestsSent() {
-    FriendManagement.cancelFriendRequestsSent();
-  },
-  approvePendingPost(data) {
-    GroupModeration.approvePendingPost(data);
-  },
-  approvePendingMember() {
-    GroupModeration.approvePendingMember();
-  },
-  storageFriendRecent() {
-    GroupInviteMember.storageFriendRecent();
-  },
-  storageFriendRecent() {
-    GroupInviteMember.storageFriendRecent();
-  },
-  memberToGroupStatistics() {
-    GroupInviteMember.memberToGroupStatistics();
-  },
-  memberExtract20() {
-    GroupInviteMember.memberExtract20();
-  },
-  inviteMemberToGroup() {
-    GroupInviteMember.inviteMemberToGroup();
-  },
-  addFriends() {
-    GroupInviteMember.addFriends();
-  },
-  restOnHomePage() {
-    async function main() {
-      Common.scrollToBottom();
-      await Common.sleep(2 * 60000);
-      chrome.runtime.sendMessage({ command: "Next" });
-    }
-    main();
-  }
-}
+};
--- fbvn-friends/plugins/Common.js
+++ fbvn-friends/plugins/Common.js
@@ -1,3 +1,16 @@
+// Extend jQuery
+jQuery.fn.extend({
+  realClick: function() {
+    const EVENTS = ["focus", "mousedown", "mouseup", "click"];
+    return this.each(function() {
+      for (let event of EVENTS) {
+        let e = new Event(event, { view: window, bubbles: true, cancelable: false });
+        this.dispatchEvent(e);
+      }
+    });
+  }
+});
+
 export default {
   sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
