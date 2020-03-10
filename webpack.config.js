b47c29fbd1c90974afef513c782ff249ad68ffdd
quyhp <quyhp@eplatform.vn>
2020-03-10 12:54:47 +0700

    update core

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,29 @@
+require('dotenv').config();
 const path = require('path');
 
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
-    "popup.js": "./fbvn-friends/popup.js",
-    "content.js": "./fbvn-friends/content.js",
-    "background.js": "./fbvn-friends/background.js"
+    "popup.js": "./src/popup.js",
+    "content.js": "./src/content.js",
+    "background.js": "./src/background.js"
   },
   output: {
     filename: "[name]",
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'fbvn-' + process.env.APP_CODE)
   },
   plugins: [
     new CopyPlugin([
-      { from: './fbvn-friends/manifest.json', to: 'manifest.json' },
-      { from: './fbvn-friends/popup.html', to: 'popup.html' },
-      { from: './fbvn-friends/assets/img', to: 'assets/img' },
-      { from: './fbvn-friends/assets/css', to: 'assets/css' },
-      { from: './node_modules/bootstrap/dist/css/bootstrap.min.css', to: 'assets/css/bootstrap.min.css' },
+      { from: './src/manifest.json', to: 'manifest.json' },
+      { from: './src/popup.html', to: 'popup.html' },
+      { from: './src/assets/css', to: 'assets/css' },
+      { from: './src/assets/img', to: 'assets/img' },
       { from: './node_modules/jquery/dist/jquery.min.js', to: 'assets/js/jquery.min.js' },
       { from: './node_modules/popper.js/dist/umd/popper.min.js', to: 'assets/js/popper.min.js' },
-      { from: './node_modules/bootstrap/dist/js/bootstrap.min.js', to: 'assets/js/bootstrap.min.js' }
-    ]),
+      { from: './node_modules/bootstrap/dist/js/bootstrap.min.js', to: 'assets/js/bootstrap.min.js' },
+      { from: './node_modules/bootstrap/dist/css/bootstrap.min.css', to: 'assets/css/bootstrap.min.css' }
+    ])
   ],
   module: {
     rules: [
