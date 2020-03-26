73f3e7ea4d6492ff869040c707be20eafed316c3
quyhp <quyhp@eplatform.vn>
2020-03-26 15:27:50 +0700

    update plugins/FriendManagement.js

--- fbvn-friends/plugins/FriendManagement.js
+++ fbvn-friends/plugins/FriendManagement.js
@@ -6,6 +6,7 @@ export default {
   },
   cancelFriendRequestsSent() {
     async function main() {
+      await Common.sleep(1000);
       let btnCancelList = $(`a[data-sigil="touchable check m-cancel-request"] button[type="submit"]:visible`);
       for (let btn of btnCancelList) {
         Common.scrollToElement(btn, undefined, 100);
