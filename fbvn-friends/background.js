dd77f61b8e347abcf3750cd0020e3bc00e4ee7ae
quyhp <tuyhpq@gmail.com>
2020-07-31 10:05:16 +0700

    update list of group
    # Conflicts:
    #	fbvn-friends/background.js

--- fbvn-friends/background.js
+++ fbvn-friends/background.js
@@ -146,8 +146,8 @@ const FEATURE = {
       { id: "694039351025214", name: "Free Fire", blackList: BLACK_LIST, rejects: REJECTS, loop: 5, notApprovePost: true },
       // { id: "qv98vn", name: "Mua bán PUBG" }, đã bán
       { id: "298297000328148", name: "COD", blackList: BLACK_LIST, rejects: REJECTS },
-      { id: "360848434395577", name: "Hỗ trợ PUBG", approves: ['acc'] },
-      { id: "744092792625338", name: "Làm quen", blackList: BLACK_LIST },
+      // { id: "360848434395577", name: "Hỗ trợ PUBG", approves: ['acc'] }, đã bán
+      { id: "744092792625338", name: "Làm quen", blackList: BLACK_LIST, notApprovePost: true },
       // { id: "108807306468805", name: "LOT" }, đã bán 
       // { id: "486751422133880", name: "Hack FF" }, đã bị khóa
       // { id: "1129744734043345", name: "Ngắm Trai" }, đã bán
@@ -160,11 +160,11 @@ const FEATURE = {
       // { id: "2621702094586454", name: "Đồ cũ Tuy Hòa" }, đã bán
       { id: "346843986016861", name: "Tus buồn", blackList: BLACK_LIST },
       // { id: "351563755753840", name: "Xe độ" }, // đã bán
-      { id: "498154793692114", name: "Buff sao Liên Quân" },
-      { id: "191718824567616", name: "Liên quân confess" },
+      // { id: "498154793692114", name: "Buff sao Liên Quân" }, cho thuê
+      { id: "191718824567616", name: "Liên quân confess", notApprovePost: true },
       // { id: "vltk2016", name: "ZSM" }, đã bán
       // { id: "danh.tuong.3q.vng", name: "Danh tướng" }, // đã bán
-      { id: "ccht.garena", name: "FF mua bán" },
+      // { id: "ccht.garena", name: "FF mua bán" }, cho thuê
       // { id: "1954247621454629", name: "FF cộng đồng" }, đã bán 
       { id: "AxE.Alliance.Empire.VN", name: "Mua bán AxE" }
     ];
